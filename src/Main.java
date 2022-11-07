c5c72cb5a3139951076f92b4fa29493749a4dd03
argen_kasymov <mistaru@bk.ru>
2022-11-07 19:54:13 +0600

    commit for local repo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Я коммитил новый коммит");
+        System.out.println("Это строка только для локального репозитория");
     }
 }
