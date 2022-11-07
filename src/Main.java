ccdbe0c162e59aad3d399cdc006bc4f1d9533f2b
argen_kasymov <mistaru@bk.ru>
2022-11-07 20:06:19 +0600

    intel commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Я коммитил новый коммит");
         System.out.println("Это строка только для локального репозитория");
+        System.out.println("Это строка только для встроенного гита");
     }
 }
